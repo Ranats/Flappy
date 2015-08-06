fec726ee154bf08eaf3d295da79479d7478abc7f
Ranats <sopurani885@gmail.com>
2015-08-06 15:25:39 +0900

    no message

---
+++ FlappySomething/Assets/Scripts/BgManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BgManager : MonoBehaviour {
+
+    public GameObject[] bg1, bg2;
+
+    GameObject[][] backgrounds;
+
+    Vector3 nextBgPos;
+    bool startFlg;
+
+	// Use this for initialization
+	void Start () {
+        startFlg = false;
+        nextBgPos = bg2[0].transform.localPosition;
+        backgrounds = new GameObject[][]{bg1,bg2};
+        for (int i = 0; i < bg1.Length; i++)
+        {
+            bg1[i].transform.SetSiblingIndex(i*2);
+            bg2[i].transform.SetSiblingIndex(i*2+1);
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startFlg = true;
+        }
+        if (startFlg)
+        {
+            BGScroller();
+        }
+	}
+
+    void BGScroller()
+    {
+        for (int j = 0; j < backgrounds.Length; j++)
+        {
+            for (int i = 0; i < bg1.Length; i++)
+            {
+                if (backgrounds[j][i].GetComponent<RectTransform>().localPosition.x <= -Screen.width / 2)
+                {
+                    nextBgPos.y = backgrounds[j][i].transform.localPosition.y;
+                    backgrounds[j][i].transform.localPosition = nextBgPos;
+                }
+                backgrounds[j][i].transform.localPosition += Vector3.left * (i + 1) * 5;
+            }
+        }
+    }
+
+}
---
+++ FlappySomething/Assets/Scripts/MapManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapManager : MonoBehaviour {
+
+    public GameObject pipePrefab,
+                      player;
+    float x,y,z,height;
+    public float rate = 10;
+
+    Vector3 playerPos;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 pos = this.transform.position;
+        y = pos.y;
+        z = pos.z;
+        playerPos = player.transform.localPosition;
+        height = 0;
+        //        StartCoroutine(CreatePipes(rate));
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+
+        if (player.transform.localPosition.x - playerPos.x > rate)
+        {
+            print(player.transform.localPosition.x - playerPos.x);
+
+            CreatePipe(height);
+            playerPos = player.transform.localPosition;
+        }
+        //        if (player.transform.position.x % scale < 0.1f)
+	}
+
+    void Move()
+    {
+        x = player.transform.position.x;
+        this.transform.position = new Vector3(x, y, z);
+    }
+
+    void CreatePipe(float y)
+    {
+        float rand = Random.Range(height-2, height+2);
+        //  bottom < rand < top
+        Mathf.Clamp(rand, -3, 4);
+        Vector3 pos = new Vector3(player.transform.position.x + 13, rand, z);
+        GameObject.Instantiate(pipePrefab, pos, pipePrefab.transform.rotation);
+    }
+
+
+    //  一定時間ごとじゃなくてPlayerのx座標をStart時に格納して、Player.transform.position.x - それ > rate(間隔)　でやっても良いかも
+    IEnumerator CreatePipes(float time)
+    {
+        while (true)
+        {   
+        float rand = Random.Range(-3, 4);
+        Vector3 pos = new Vector3(player.transform.position.x +13,rand,z);
+        GameObject.Instantiate(pipePrefab, pos, pipePrefab.transform.rotation);
+        yield return new WaitForSeconds(time);
+        }
+    }
+}
